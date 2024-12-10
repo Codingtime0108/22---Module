01641b8be02ca020f0e06ffcb44c634659ab3f39
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-12-10 18:22:37 +0900

    hour

---
+++ hour.py
@@ -0,0 +1,6 @@
+import datetime
+Time = datetime.datetime.now()
+print("Hour = ", Time.hour)
+print("Minute = ", Time.minute)
+print("Second = ", Time.second)
+print("Microsecond = ", Time.microsecond)
