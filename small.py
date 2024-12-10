2e5f07abcba6316a4549bf634d45223bef636e70
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-12-10 18:16:31 +0900

    today

---
+++ small.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+today = date.today()
+print("Today's date is", today)
+print("Current month is", today.month)
+print("Current day is", today.day)
+print("Current year is", today.year)
