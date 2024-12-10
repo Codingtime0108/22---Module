bfff588b1392545e177600d9cd8bb213ab5536c1
Codingtime <13tia.vora@dulwich-seoul.kr>
2024-12-10 18:33:22 +0900

    hw

---
+++ month.py
@@ -0,0 +1,29 @@
+#display month names
+from datetime import date
+names = date.today()
+answer = names.month
+
+if answer == 12:
+    print("December")
+if answer == 11:
+    print("November")
+if answer == 10:
+    print("October")
+if answer == 9:
+    print("September")
+if answer == 8:
+    print("August")
+if answer == 7:
+    print("July")
+if answer == 6:
+    print("June")
+if answer == 5:
+    print("April")
+if answer == 4:
+    print("May")
+if answer == 3:
+    print("March")
+if answer == 2:
+    print("February")
+if answer == 1:
+    print("January")
